2a1102a87cafeacb9d0c6c5e9d35f27e52354c6c
tai <zzwzzhao@163.com>
2013-05-31 16:01:50 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaiBlog::Application.config.secret_token = '7741d4a5f326266285631848630b3a0875b024085091d432d0f68b1b405b8c11dbf47de408d82722fe3f432085d37031418271325b6f67790344803b6676cdf7'
